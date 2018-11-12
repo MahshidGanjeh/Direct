0cc9f9b02d16fa4461153e0240e11e2f735ba0b3
Hp <mahshidga@gmail.com>
2018-11-12 10:02:51 +0330

    initial commit splash (screen activity)

---
+++ app/src/main/java/com/yara/direct/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.yara.direct;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
